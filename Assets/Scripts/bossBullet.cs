0d221c920e8804f2f3eb418d9c2a7de059017938
tianBreznik <37045546+tianBreznik@users.noreply.github.com>
2019-06-25 23:47:23 +0200

    WORKS

--- Assets/Scripts/bossBullet.cs
+++ Assets/Scripts/bossBullet.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class bossBullet : MonoBehaviour
@@ -9,13 +10,22 @@ public class bossBullet : MonoBehaviour
     private Vector2 moveDirection;
     public float timeToKill = 5;
 
+    //private bossPatternController instance;
+    private enemy boss;
+    //public static bossBullet instance;
+
+
+    private void Start()
+    {
+        boss = gameObject.GetComponent<enemy>();
+    }
 
     private void OnEnable()
     {
         Invoke("Destroy", timeToKill);
     }    
 
-    private void Update()
+    void Update()
     {
         transform.Translate(moveDirection * speed * Time.deltaTime);
     }
@@ -31,9 +41,28 @@ public class bossBullet : MonoBehaviour
         gameObject.SetActive(false);
     }
     
-
+    public void setSpeed(float set_speed)
+    {
+        speed = set_speed;
+    }
+    
+    public float getSpeed()
+    {
+        return speed;
+    }
     private void OnDisable()
     {
         CancelInvoke();
+    }
+
+    public void setBulletSpeed(enemy boss)
+    {
+        int health = boss.getHealth();
+        if (Enumerable.Range(1, 3).Contains(health))
+            speed = 11f;
+        else if (Enumerable.Range(3, 7).Contains(health))
+            speed = 6f;
+        else if (Enumerable.Range(7, 10).Contains(health))
+            speed = 2f;
     }
 }
